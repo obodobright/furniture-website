9a7066b48a90cfc951d597e8a9662d55866afcbf
Bright-Lumen <72477636+obodobright@users.noreply.github.com>
2021-01-31 17:50:52 +0100

    Add files via upload

---
+++ furniture website/app.js
@@ -0,0 +1,16 @@
+const showMenu = () => {
+    document.getElementById('nav-list').style.display = "block"
+    document.getElementById('toggle-icon').style.display = "none"
+    document.getElementById('navbar-brand').style.display = "none"
+
+}
+const hideMenu = () => {
+    document.getElementById('nav-list').style.display = "none"
+    document.getElementById('toggle-icon').style.display = "block"
+    document.getElementById('navbar-brand').style.display = "block"
+}
+
+
+// function showMenu() {
+//     document.getElementById('nav-list').style.display = "block"
+// }
